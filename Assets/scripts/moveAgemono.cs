121574226ade71872c552aa6beb9ff8b49196118
nagochan <41363672+nagochan@users.noreply.github.com>
2020-02-22 21:46:17 +0800

    ゲームの基本的なシステムを作った
    あとはバーを調整したい

--- Assets/scripts/moveAgemono.cs
+++ Assets/scripts/moveAgemono.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class moveAgemono : MonoBehaviour
 {
-    [SerializeField] float forawdSpeed = 0.1f;
+    [SerializeField] float forawdSpeed = 0.1f; 
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //create Enemy.
+        StartCoroutine("createEnemy");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, forawdSpeed);
+        //move Bar.
+        barMove();
+
+    }
+
+    void barMove()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(-forawdSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(forawdSpeed, 0, 0);
+        }
+    }
+
+    IEnumerator createEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            float randomX = Random.Range(-4, 4);
+            Vector3 enemyPos = new Vector3(randomX, 0.5f, -24f);
+            GameObject enemyObj = Instantiate(enemy, enemyPos, Quaternion.identity);
+            enemyObj.name = "enemy";
+        }
     }
 }
---
+++ Assets/scripts/moveEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveEnemy : MonoBehaviour {
+	[SerializeField] float enemySpeed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, -enemySpeed);
+		Destroy(this.gameObject, 10);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("kill me!");
+		Destroy(this.gameObject);
+		// 3D同士が接触した瞬間の１回のみ呼び出される処理
+	}
+}
