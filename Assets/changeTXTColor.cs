588acbfdc189ba0a6cdaa3e0e97b0edf1476ec6e
nagochan <41363672+nagochan@users.noreply.github.com>
2020-02-23 15:24:15 +0800

    ゲームオーバーの文字色を変えるプログラムを書きかけた

---
+++ Assets/changeTXTColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeTXTColor : MonoBehaviour
+{
+    Text color;
+    bool colorbool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = this.GetComponent<Text>();
+        StartCoroutine("ChangeColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ChangeColor()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            if (colorbool)
+            {
+                color.color = new Color(1.0f, 0, 0, 1);
+                colorbool = false;
+                Debug.LogError("change!");
+            }
+            else
+            {
+                color.color = new Color(1, 1, 1, 1);
+                colorbool = true;
+            }
+        }
+    }
+}
